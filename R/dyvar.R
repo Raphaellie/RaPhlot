b2c37b6e60292f4c1f05e775bb187e8deab1814b
Raphael Wang <chyrwang@gmail.com>
2022-11-01 12:46:04 +0800

    Update dyvar.R

--- R/dyvar.R
+++ R/dyvar.R
@@ -11,27 +11,27 @@
 #' @import viridisLite
 
 
-dyvar <- function(data,var,time_id, years = seq(1952,2020,4) ){
+dyvar <- function(data,var,time_id,group = NULL, breaks = seq(1952,2020,4) ){
 
 
 results <-
   data %>%
   mutate(time = get(time_id), var2 = get(var)) %>%
   filter(time %in% years) %>%
-  group_by(time) %>%
+  group_by(time, get(group)) %>%
   summarise(estimate = mean(var2,na.rm = T),
             sd = sd(var2,na.rm = T),
             n = sum(!is.na(var2)),
             std.err = sd/sqrt(n),
             conf.high = estimate + qnorm(0.975)*std.err,
             conf.low  = estimate - qnorm(0.975)*std.err ) %>%
-  filter(!is.na(estimate))
+  filter(!is.na(estimate),!is.na(get(group)))
 
 plot <-
   results %>%
-  ggplot(aes(x = time, y = estimate, ymin = conf.low, ymax = conf.high)) +
+  ggplot(aes(x = time, y = estimate, ymin = conf.low, ymax = conf.high, color = get(group))) +
   geom_pointrange(position = position_dodge(width = 2)) +
-  scale_x_continuous(breaks = seq(1952,2020,4)) +
+  scale_x_continuous(breaks = years) +
   scale_color_brewer(palette = 'Set1') +
   theme_minimal() +
   theme(legend.position = 'none')
